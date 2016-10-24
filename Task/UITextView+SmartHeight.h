f3ac10f2daf47fc8bb9b0128ea00ce4dd507d9b6
Petko Todorov <petkotodorov@ORYXBG064.local>
2016-10-24 19:12:28 +0300

    moving files

---
+++ Task/UITextView+SmartHeight.h
@@ -0,0 +1,13 @@
+//
+//  UITextView+SmartHeight.h
+//  Task
+//
+//  Created by Petko Todorov on 9/13/16.
+//  Copyright © 2016 Petko Todorov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextView (SmartHeight)
+
+@end
